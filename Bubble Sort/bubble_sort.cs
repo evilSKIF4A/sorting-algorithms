8e305633d7d6eaf4343ae0c6b5f73db10219b17c
Dmitry <49763101+evilSKIF4A@users.noreply.github.com>
2022-06-14 23:30:51 +0500

    Add files via upload

---
+++ Bubble Sort/bubble_sort.cs
@@ -0,0 +1,12 @@
+internal static int[] bubble_sort(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+        for(int j = 1; j < arr.Length; j++)
+            if (arr[j] < arr[j - 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
+    return arr;
+}

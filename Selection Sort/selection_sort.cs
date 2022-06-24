6263abba0a3ffb88a20ed885407e1d7c811f314a
Dmitry <49763101+evilSKIF4A@users.noreply.github.com>
2022-06-25 00:23:55 +0500

    Add files via upload

---
+++ Selection Sort/selection_sort.cs
@@ -0,0 +1,16 @@
+internal static int[] selection_sort(int[] arr)
+{
+    for(int i = 0; i < arr.Length; ++i)
+    {
+        int k = i;
+        for(int j = i + 1; j < arr.Length; ++j)
+        {
+            if (arr[k] > arr[j])
+                k = j;
+        }
+        int temp = arr[i];
+        arr[i] = arr[k];
+        arr[k] = temp;
+    }
+    return arr;
+}

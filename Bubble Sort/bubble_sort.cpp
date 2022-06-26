f78b5a3fa0db4b769a28689d3736bf5c6a64f021
Dmitry <49763101+evilSKIF4A@users.noreply.github.com>
2022-06-26 22:34:53 +0500

    C++

--- Bubble Sort/bubble_sort.cpp
+++ Bubble Sort/bubble_sort.cpp
@@ -1,4 +1,4 @@
-void bubble_sort(vector<int>& arr) {
+void BubbleSort(vector<int>& arr) {
     for (int i = 0; i < arr.size(); i++)
         for (int j = 1; j < arr.size(); j++)
             if (arr[j] < arr[j - 1])

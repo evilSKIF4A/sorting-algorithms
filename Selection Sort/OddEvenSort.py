154917c7837e66efc251c6d6753026fe9342d82b
Dmitry <49763101+evilSKIF4A@users.noreply.github.com>
2022-06-27 01:28:18 +0500

    Delete OddEvenSort.py

--- Selection Sort/OddEvenSort.py
+++
@@ -1,6 +0,0 @@
-def OddEvenSort(arr):
-    for i in range(len(arr)):
-        for j in range(0 if i % 2 == 0 else 1, len(arr), 2):
-            if j + 1 < len(arr) and arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr

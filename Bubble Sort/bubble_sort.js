7271c1abf5f65900df8295eb80c9bf06bcebe3c4
Dmitry <49763101+evilSKIF4A@users.noreply.github.com>
2022-06-26 22:24:47 +0500

    JavaScript

---
+++ Bubble Sort/bubble_sort.js
@@ -0,0 +1,6 @@
+const BubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; ++i)
+    for (let j = 1; j < arr.length; ++j)
+      if (arr[j] < arr[j - 1]) [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+  return arr;
+};

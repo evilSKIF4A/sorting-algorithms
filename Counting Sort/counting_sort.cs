44c14d3422020295a783e4e041cea236ba78ac5a
Dmitry <49763101+evilSKIF4A@users.noreply.github.com>
2022-06-27 00:09:15 +0500

    C#

--- Counting Sort/counting_sort.cs
+++ Counting Sort/counting_sort.cs
@@ -1,14 +1,21 @@
-internal static int[] counting_sort(int[] arr, int n)
+internal static int MaximumNumber(int[] arr)
 {
+    if (arr.Length == 0) return 0;
+    int max = arr[0];
+    for (int i = 1; i < arr.Length; ++i)
+        if (max < arr[i])
+            max = arr[i];
+    return max;
+}
+internal static int[] CountingSort(int[] arr)
+{
+    int temp = MaximumNumber(arr);
+    int n = arr.Length > temp ? arr.Length : temp + 1;
     int[] tempArr = new int[n];
-    for (int i = 0; i < n; i++) tempArr[arr[i]]++;
+    for (int i = 0; i < arr.Length; i++) tempArr[arr[i]]++;
     int j = 0;
     for (int i = 0; i < n; i++)
-    {
         while (tempArr[i]-- != 0)
-        {
             arr[j++] = i;
-        }
-    }
     return arr;
 }
